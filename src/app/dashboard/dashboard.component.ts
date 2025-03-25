0160490bf57f0cfdd7e3f3060c42843d04de7479
TANTELY00 <tanteli.ia.engineer@gmail.com>
2025-03-25 21:37:41 +0300

    vf2

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // importer pour utiliser *ngFor
+import { CommonModule} from '@angular/common'; // importer pour utiliser *ngFor
+import { provideAnimations } from '@angular/platform-browser/animations'; // importer pour utiliser les animations
+import {
+  ModalComponent,
+  ModalHeaderComponent,
+  ModalBodyComponent,
+  ModalFooterComponent,
+  ButtonDirective,
+  ButtonCloseDirective,
+  ModalTitleDirective
+} from '@coreui/angular';
+
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'] // aux pluriels
 })
 export class DashboardComponent {
 
   // DECLARATIONS DES VARIABLES UTILISEES
   tableauDesInfoDash : any[] = [
     {image: "1.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
+    {image: "2.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
+    {image: "3.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
+    {image: "4.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
     {image: "5.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
-    {image: "6.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
-    {image: "8.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" },
-    {image: "10.png", description: "" , nomPoste: "" , experience: "" , date_pub: "" }
   ];
 
   index1 : number = 0;
@@ -38,6 +50,17 @@ export class DashboardComponent {
   ngOnInit() {
     console.log(this.tab.length)
   }
+
+  public visible = false;
+
+  toggleModal() {
+    this.visible = !this.visible;
+  }
+
+  handleVisibleChange(event: boolean) {
+    this.visible = event;
+  }
+
 
   previous(){
     this.currentVariable = this.tab.length - 1;
@@ -67,5 +90,6 @@ export class DashboardComponent {
     this.v3 = this.tableauDesInfoDash[this.index3];
  
   }
+ 
 
 }
